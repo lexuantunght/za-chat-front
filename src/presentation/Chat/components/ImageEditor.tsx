1f95d0b36da9f9858bbfdfb70f53cf31c7b80db5
TUNGLX2\MININT-Q0DLGRA-local <tunglx2@vng.com.vn>
2022-07-13 09:53:25 +0700

    change editor to scaleflex-editor

--- src/presentation/Chat/components/ImageEditor.tsx
+++ src/presentation/Chat/components/ImageEditor.tsx
@@ -1,71 +1,44 @@
 import React from 'react';
-import 'tui-image-editor/dist/tui-image-editor.css';
-import Editor from '@toast-ui/react-image-editor';
-import Button from '../../../common/components/Button';
-import Icon from '../../../common/components/Icon';
+import FilerobotImageEditor, { TABS, TOOLS } from 'react-filerobot-image-editor';
 
 type ImageEditorProps = {
     imagePath?: string;
     imageName?: string;
-    acceptLabel?: string;
-    cancelLabel?: string;
     onCancel?: () => void;
     onAccept?: (dataURL?: string) => void;
 };
 
-const ImageEditor = ({
-    imagePath,
-    imageName,
-    onCancel,
-    onAccept,
-    acceptLabel = 'Done',
-    cancelLabel = 'Cancel',
-}: ImageEditorProps) => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const editorRef = React.useRef<any>(null);
+const ImageEditor = ({ imagePath, imageName, onCancel, onAccept }: ImageEditorProps) => {
     return (
         <div className="chat-image-editor">
             <div className="chat-image-editor-wrapper">
-                <Editor
-                    ref={editorRef}
-                    includeUI={{
-                        menu: ['crop', 'flip', 'rotate', 'draw', 'shape', 'icon', 'text', 'filter'],
-                        initMenu: 'draw',
-                        menuBarPosition: 'top',
-                        uiSize: {
-                            width: '100%',
-                            height: '100%',
-                        },
-                        loadImage:
-                            imagePath && imageName
-                                ? { path: imagePath, name: imageName }
-                                : undefined,
+                <FilerobotImageEditor
+                    source={imagePath || ''}
+                    defaultSavedImageName={imageName}
+                    defaultSavedImageType="jpeg"
+                    onSave={(editedImageObject) => {
+                        onAccept?.(editedImageObject.imageBase64);
                     }}
-                    cssMaxHeight={400}
-                    usageStatistics={false}
-                    selectionStyle={{
-                        cornerSize: 10,
-                        rotatingPointOffset: 50,
+                    onClose={onCancel}
+                    previewPixelRatio={window.devicePixelRatio}
+                    savingPixelRatio={4}
+                    avoidChangesNotSavedAlertOnLeave={false}
+                    closeAfterSave
+                    annotationsCommon={{
+                        fill: '#ff0000',
                     }}
+                    Text={{ text: 'ZaChat...' }}
+                    tabsIds={[
+                        TABS.ADJUST,
+                        TABS.FILTERS,
+                        TABS.RESIZE,
+                        TABS.FINETUNE,
+                        TABS.ANNOTATE,
+                        TABS.WATERMARK,
+                    ]}
+                    defaultTabId={TABS.ANNOTATE}
+                    defaultToolId={TOOLS.TEXT}
                 />
-            </div>
-            <div className="chat-image-editor-buttons">
-                <Button variant="outlined" onClick={onCancel}>
-                    <Icon name="cancel" />
-                    <span>{cancelLabel}</span>
-                </Button>
-                <Button
-                    className="chat-image-editor-done"
-                    onClick={() =>
-                        onAccept?.(
-                            editorRef.current
-                                ?.getInstance()
-                                .toDataURL({ format: 'jpeg', quality: 0.5 })
-                        )
-                    }>
-                    <Icon name="check" />
-                    <span>{acceptLabel}</span>
-                </Button>
             </div>
         </div>
     );
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,3 @@
 declare module '*.module.css';
 declare module '*.module.scss';
 declare module '*.png';
-
-declare module '@toast-ui/react-image-editor';
